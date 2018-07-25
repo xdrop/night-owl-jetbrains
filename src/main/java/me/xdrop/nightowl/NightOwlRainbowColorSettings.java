a44a7d8b7f2a2c7e2f18598634d811a09fbefa7f
xdrop <xdrop.me@gmail.com>
2018-07-25 19:55:30 +0100

    Remove API dependency on rainbow brackets (See #6)

--- src/main/java/me/xdrop/nightowl/NightOwlRainbowColorSettings.java
+++ src/main/java/me/xdrop/nightowl/NightOwlRainbowColorSettings.java
@@ -1,18 +1,40 @@
 package me.xdrop.nightowl;
 
-import com.github.izhangzhihao.rainbow.brackets.settings.RainbowSettings;
+import com.intellij.configurationStore.StateStorageManager;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceKt;
+import com.intellij.openapi.diagnostic.Logger;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class NightOwlRainbowColorSettings {
+    private static Logger logger = Logger.getInstance(NightOwlRainbowColorSettings.class);
+
     public static void applyRainbowColorSettings() {
-        RainbowSettings settings = RainbowSettings.Companion.getInstance();
-        settings.setEnableRainbowRoundBrackets(true);
-        settings.setEnableRainbowAngleBrackets(false);
-        settings.setEnableRainbowSquareBrackets(true);
-        settings.setEnableRainbowSquigglyBrackets(true);
-        settings.setRainbowifyHTMLInsideJS(true);
-        settings.setRainbowifyKotlinFunctionLiteralBracesAndArrow(true);
-        settings.setDarkRoundBracketsColors(new String[]{"0xffd700","0xda70d6","0x87cefa"});
-        settings.setDarkSquareBracketsColors(new String[]{"0xffd700","0xda70d6","0x87cefa"});
-        settings.setDarkSquigglyBracketsColors(new String[]{"0xffd700","0xda70d6","0x87cefa"});
+        Application application = ApplicationManager.getApplication();
+        StateStorageManager storageManager = ServiceKt.getStateStore(application).getStorageManager();
+        String configPath = storageManager.expandMacros("$APP_CONFIG$") + "/rainbow_brackets.xml";
+        Path path = Paths.get(configPath);
+        File rainbowSettingsOut = path.toFile();
+        ClassLoader classLoader = NightOwlRainbowColorSettings.class.getClassLoader();
+        InputStream rainbowSettingsIn = classLoader.getResourceAsStream("colors/rainbow_brackets.xml");
+        replaceFile(rainbowSettingsIn, rainbowSettingsOut);
+    }
+
+    private static void replaceFile(InputStream in, File out) {
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(out))) {
+            try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(in))) {
+                String inLine;
+                while ((inLine = fileReader.readLine()) != null) {
+                    fileWriter.write(inLine);
+                    fileWriter.newLine();
+                }
+            }
+        } catch (IOException e) {
+           logger.error("Failed to replace rainbow_brackets.xml: " + e.getMessage());
+        }
     }
 }
