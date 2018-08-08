0168ac0e26f7de9bb88236eecfd8b536990c97c3
xdrop <xdrop.me@gmail.com>
2018-08-08 18:10:44 +0100

    Rename language colors and point to default for JS

---
+++ src/main/kotlin/me/xdrop/nightowl/settings/colors/NightOwlJavascriptColorSettings.kt
@@ -0,0 +1,22 @@
+package me.xdrop.nightowl.settings.colors
+
+import com.intellij.codeInsight.daemon.impl.GeneralHighlightingPass
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import me.xdrop.nightowl.highlight.NightOwlJSAnnotator
+import me.xdrop.nightowl.settings.NightOwlColorSettingsPage
+
+class NightOwlJavascriptColorSettings : NightOwlColorSettingsPage() {
+    override fun getDisplayName(): String = "Javascript/Typescript - Extended"
+
+    override fun getAttributeDescriptors(): Array<AttributesDescriptor> {
+        return DESCRIPTORS
+    }
+
+    companion object {
+        private var DESCRIPTORS = arrayOf(
+                AttributesDescriptor("Keyword: this, super", NightOwlJSAnnotator.JS_THIS_SUPER),
+                AttributesDescriptor("Keyword: export, default, module", NightOwlJSAnnotator.JS_MODULE_KEYWORD),
+                AttributesDescriptor("Keyword: debugger", NightOwlJSAnnotator.JS_DEBUGGER)
+        )
+    }
+}
